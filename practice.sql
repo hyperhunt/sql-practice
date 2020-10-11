7face35d56fe5768519c9191475d6a02da399bbc
Tony <63557005+hyperhunt@users.noreply.github.com>
2020-10-11 21:53:41 +0300

    Done task SELECT DISTINCT

--- practice.sql
+++ practice.sql
@@ -54,6 +54,36 @@ INSERT INTO PRODUCTS (product_id, price, category) VALUES (34, 28, 3);
 
 -- DROP TABLE if exists PRODUCTS;
 
+/*
+CREATE TABLE products (
+  product_id bigint,
+  price numeric(15,5),
+  name varchar(255),
+  category tinyint,
+  sup_id bigint,
+  PRIMARY KEY (product_id),
+  FOREIGN KEY (sup_id) REFERENCES suppliers(sup_id)
+);
+
+CREATE TABLE suppliers (
+  sup_id bigint,
+  city varchar (255),
+  opendate date,
+  PRIMARY KEY (sup_id)
+);
+*/
+
+-- INSERT INTO products (product_id, price, name, category, sup_id) VALUES (4, 338.88, "Forester", "Game", 4);
+-- INSERT INTO suppliers (sup_id, city, opendate) VALUES (4, "USA", 20061210);
+
+-- UPDATE products SET sup_id = 4 WHERE name = "Forester";
+-- UPDATE PRODUCTS SET price = price * 3 WHERE category = 3;
+-- FROM_UNIXTIME(time) AS formatted_date
+-- SELECT t1.*, t2.* FROM t1 JOIN t2 ON t1.i1 = t2.i2
+-- LEFT JOIN table2 as user1 on user1.ID = table1.userID1demo
+
+-- SELECT DISTINCT name from products, suppliers WHERE products.sup_id = suppliers.sup_id AND suppliers.city = 'Moscow';
+-- WHERE table1.userID1 = t2.id AND table1.userID2 = t3.id;
 
 
 
